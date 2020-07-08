ebd7490d356718bf76de8c4fe51839da36e908e4
bparedes21 <68014465+bparedes21@users.noreply.github.com>
2020-07-08 11:55:17 -0300

    nuevo apellido

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,6 +1,6 @@
 /*
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+se debe mostrar un mensaje como el siguiente "apellido"*/
 function mostrar()
 {
 	alert("paredes");
